def73f42c4098106acce2aa30085c5209cebed57
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2021-08-20 15:30:06 +0200

    Abstracted renderer further, to lay the foundation for rendering multiple objects.

--- Zayker Game Engine/Zayker Game Engine/Modules/Renderer/Renderer.cs
+++ Zayker Game Engine/Zayker Game Engine/Modules/Renderer/Renderer.cs
@@ -52,40 +52,25 @@ namespace Zayker_Game_Engine.Modules.Renderer
         private static GL Gl;
         Dictionary<string, uint> shaders = new Dictionary<string, uint>();
 
-        private static uint Vbo;
-        private static uint Ebo;
-        private static uint Vao;
-
-        /*
-        //Vertex shaders are run on each vertex.
-        private static readonly string VertexShaderSource = @"
-        #version 330 core //Using version GLSL version 3.3
-        layout (location = 0) in vec4 vPos;
-        void main()
-        {
-            gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
-        }
-        ";
-
-        //Fragment shaders are run on each fragment/pixel of the geometry.
-        private static readonly string FragmentShaderSource = @"
-        #version 400 core
-        out vec4 FragColor;
-        void main()
-        {
-            FragColor = vec4(gl_FragCoord.x/500, gl_FragCoord.y/500, 0.0f, 1.0f);
-        }
-        ";
-        */
+        private static uint VaoA;
+        private static uint VaoB;
 
         //Vertex data, uploaded to the VBO.
-        private static readonly float[] Vertices =
+        private static readonly float[] VerticesA =
         {
             //X    Y      Z
-             0.5f,  0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f,  0.5f, 0.6f
+             0.5f,  0.9f, 0.0f,
+             0.5f,  0.1f, 0.0f,
+            -0.5f,  0.1f, 0.0f,
+            -0.5f,  0.9f, 0.6f
+        };
+        private static readonly float[] VerticesB =
+        {
+            //X    Y      Z
+             0.5f, -0.1f, 0.0f,
+             0.5f, -0.9f, 0.0f,
+            -0.5f, -0.9f, 0.0f,
+            -0.5f, -0.1f, 0.6f
         };
 
         //Index data, uploaded to the EBO.
@@ -111,38 +96,55 @@ namespace Zayker_Game_Engine.Modules.Renderer
             window.Initialize();
         }
 
-        private unsafe void OnLoad()
+        unsafe uint CreateVertexArrayObject(float[] vertices, uint[] indices)
         {
 
-            //Getting the opengl api for drawing to the screen.
-            Gl = GL.GetApi(window);
-
-            //Creating a vertex array.
-            Gl.CreateVertexArrays(1, out Vao);
-            Gl.BindVertexArray(Vao);
+            //Creating the vertex array, storing all data. 
+            uint vao;
+            Gl.CreateVertexArrays(1, out vao);
+            Gl.BindVertexArray(vao);
 
             //Initializing a vertex buffer that holds the vertex data.
-            Gl.CreateBuffers(1, out Vbo); //Creating the buffer.
-            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, Vbo); //Binding the buffer.
-            fixed (void* v = &Vertices[0])
+            uint vbo;
+            Gl.CreateBuffers(1, out vbo);
+            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+            fixed (void* v = &vertices[0])
             {
-                Gl.BufferData(BufferTargetARB.ArrayBuffer, (uint)(Vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw); //Setting buffer data.
+                Gl.BufferData(BufferTargetARB.ArrayBuffer, (uint)(vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw); //Setting buffer data.
             }
 
             //Initializing a element buffer that holds the index data.
-            Gl.CreateBuffers(1, out Ebo); //Creating the buffer.
-            Gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, Ebo); //Binding the buffer.
-            fixed (void* i = &Indices[0])
+            uint ebo;
+            Gl.CreateBuffers(1, out ebo); //Creating the buffer.
+            Gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, ebo); //Binding the buffer.
+            fixed (void* i = &indices[0])
             {
-                Gl.BufferData(BufferTargetARB.ElementArrayBuffer, (uint)(Indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw); //Setting buffer data.
+                Gl.BufferData(BufferTargetARB.ElementArrayBuffer, (uint)(indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw); //Setting buffer data.
             }
-
-            GenerateShaderFromFile("default", Program.modulesDirectory+ "Renderer/BuiltInShaders/BuiltInShader.vert", 
-                                              Program.modulesDirectory + "Renderer/BuiltInShaders/BuiltInShader.frag");
 
             //Tell opengl how to give the data to the shaders.
             Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
             Gl.EnableVertexAttribArray(0);
+
+            // vbo and ebo should be deleted when the program stops. I cant do that right now, because there are no references passed out. 
+            // Gl.DeleteBuffer(vbo);
+            // Gl.DeleteBuffer(ebo);
+
+            return vao;
+        }
+
+        private unsafe void OnLoad()
+        {
+            //Getting the opengl api for drawing to the screen.
+            Gl = GL.GetApi(window);
+
+            // Generate the engines default shader
+            GenerateShaderFromFile("default", Program.modulesDirectory + "Renderer/BuiltInShaders/BuiltInShader.vert",
+                                              Program.modulesDirectory + "Renderer/BuiltInShaders/BuiltInShader.frag");
+
+            VaoA = CreateVertexArrayObject(VerticesA, Indices);
+            VaoB = CreateVertexArrayObject(VerticesB, Indices);
+
         }
 
         private unsafe void OnRender(double obj)
@@ -151,11 +153,20 @@ namespace Zayker_Game_Engine.Modules.Renderer
             Gl.Clear((uint)ClearBufferMask.ColorBufferBit);
 
             //Bind the geometry and shader.
-            Gl.BindVertexArray(Vao);
+            Gl.BindVertexArray(VaoA); // We already bound this ealier
             Gl.UseProgram(shaders["default"]);
 
             //Draw the geometry.
             Gl.DrawElements(GLEnum.Triangles, (uint)Indices.Length, GLEnum.UnsignedInt, (void*)0);
+
+            //Bind the geometry and shader.
+            Gl.BindVertexArray(VaoB); // We already bound this ealier
+            Gl.UseProgram(shaders["default"]);
+
+            //Draw the geometry.
+            Gl.DrawElements(GLEnum.Triangles, (uint)Indices.Length, GLEnum.UnsignedInt, (void*)0);
+
+            Gl.BindVertexArray(0); // Why? I added this and its not needed. Might just be good practice.
         }
 
         private void OnUpdate(double obj)
@@ -165,9 +176,8 @@ namespace Zayker_Game_Engine.Modules.Renderer
 
         private void OnClose()
         {
-            Gl.DeleteBuffer(Vbo);
-            Gl.DeleteBuffer(Ebo);
-            Gl.DeleteVertexArray(Vao);
+            Gl.DeleteVertexArray(VaoA);
+            Gl.DeleteVertexArray(VaoB);
             foreach (uint shader in shaders.Values)
             {
                 Gl.DeleteProgram(shader);
